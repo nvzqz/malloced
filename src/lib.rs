5a2e6379258d34efe317d6253e7016361ba49603
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-12 15:40:30 -0500

    Implement `Drop` for `Malloced`

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 #![no_std]
 
-use core::ptr::NonNull;
+use core::{
+    ffi::c_void,
+    ptr::{self, NonNull},
+};
 
 /// A pointer type for `malloc`-ed heap allocation.
 #[repr(transparent)]
 pub struct Malloced<T: ?Sized> {
     ptr: NonNull<T>,
+}
+
+impl<T: ?Sized> Drop for Malloced<T> {
+    #[inline]
+    fn drop(&mut self) {
+        extern "C" {
+            fn free(ptr: *mut c_void);
+        }
+
+        let ptr = self.ptr.as_ptr();
+        unsafe {
+            ptr::drop_in_place(ptr);
+            free(ptr as *mut c_void);
+        }
+    }
 }
