d3af9257aff179a2098009c1fc1998773c6ffecb
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-12 17:35:48 -0500

    Create `Malloced::slice_from_raw_parts`

--- src/lib.rs
+++ src/lib.rs
@@ -85,3 +85,45 @@ impl<T: ?Sized> Malloced<T> {
         }
     }
 }
+
+impl<T> Malloced<[T]> {
+    /// Constructs an instance for a slice from a pointer and a length.
+    ///
+    /// # Safety
+    ///
+    /// Behavior is undefined if any of the following conditions are violated:
+    ///
+    /// - `data` must have been allocated by `malloc` so that it can be `free`-d
+    ///   on [`Drop`](https://doc.rust-lang.org/std/ops/trait.Drop.html).
+    ///
+    /// - `data` must be
+    ///   [valid](https://doc.rust-lang.org/std/ptr/index.html#safety) for both
+    ///   reads and writes for `len * mem::size_of::<T>()` many bytes, and it
+    ///   must be properly aligned. This means in particular:
+    ///
+    ///     - The entire memory range of this slice must be contained within a
+    ///       single allocated object! Slices can never span across multiple
+    ///       allocated objects.
+    ///
+    ///     - `data` must be non-null and aligned even for zero-length slices.
+    ///       One reason for this is that enum layout optimizations may rely on
+    ///       references (including slices of any length) being aligned and
+    ///       non-null to distinguish them from other data. You can obtain a
+    ///       pointer that is usable as `data` for zero-length slices using
+    ///       [`NonNull::dangling()`](https://doc.rust-lang.org/std/ptr/struct.NonNull.html#method.dangling).
+    ///
+    /// - `data` must point to `len` consecutive properly initialized values of
+    ///   type `T`.
+    ///
+    /// - The total size `len * mem::size_of::<T>()` of the slice must be no
+    ///   larger than `isize::MAX`. See the safety documentation of
+    ///   [`pointer::offset`](https://doc.rust-lang.org/std/primitive.pointer.html#method.offset).
+    ///
+    /// See
+    /// [`slice::from_raw_parts_mut`](https://doc.rust-lang.org/std/slice/fn.from_raw_parts_mut.html)
+    /// for details.
+    #[inline]
+    pub unsafe fn slice_from_raw_parts(data: *mut T, len: usize) -> Self {
+        Self::from_raw(core::slice::from_raw_parts_mut(data, len))
+    }
+}
