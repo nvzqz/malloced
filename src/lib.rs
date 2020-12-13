aef6efc55e8c4dbd7ab3891664b99be39f2c96cc
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-12 23:18:19 -0500

    Prepare 1.0.0 release

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! malloced = "0.0.0"
+//! malloced = "1.0.0"
 //! ```
 //!
 //! The star of the show is [`Malloced`], [`Box`]-like pointer that calls `free` on
